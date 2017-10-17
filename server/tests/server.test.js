7e09fcbb67ccfa3297c9921c547ab600e0d76f4b
Jonathan Chiang <signup.jc13@gmail.com>
2017-10-17 14:25:54 -0500

    add tests for GET /tasks

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,14 @@ const request = require('supertest');
 const app = require('./../server.js').app;
 const Task = require('./../models/Task.js').Task;
 
+const TaskStore = [{
+  title: 'First test task',
+}, {
+  title: 'Second test task',
+}];
+
 beforeEach((done) => {
-  Task.remove({}).then(() => done());
+  Task.remove({}).then(() => Task.insertMany(TaskStore)).then(() => done());
 });
 
 describe('POST /tasks', () => {
@@ -26,7 +32,9 @@ describe('POST /tasks', () => {
           return done(err);
         }
 
-        Task.find().then((tasks) => {
+        Task.find({
+          title: text,
+        }).then((tasks) => {
           expect(tasks.length).toBe(1);
           expect(tasks[0].title).toBe(text);
           done();
@@ -44,9 +52,21 @@ describe('POST /tasks', () => {
           return done(err);
         }
         Task.find().then((tasks) => {
-          expect(tasks.length).toBe(0);
+          expect(tasks.length).toBe(2);
           done();
         }).catch(e => done(e));
       });
+  });
+});
+
+describe('GET /tasks', () => {
+  it('should get all tasks', (done) => {
+    request(app)
+      .get('/tasks')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.tasks.length).toBe(2);
+      })
+      .end(done);
   });
 });
